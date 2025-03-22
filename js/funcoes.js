c8540a8e13385b3f823ee6d06831b2842a90b9d7
jhessycaborges <jhessyca.borgez@outlook.com>
2025-03-22 13:15:05 -0300

    Atualização Js

--- js/funcoes.js
+++ js/funcoes.js
@@ -3,6 +3,21 @@ function alterarQtd(produto, acao) {
     const valor = document.getElementById('valor' + produto);
     const total = document.getElementById('total' + produto);
 
-    acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--;
-    total.innerHTML = (qtd.innerHTML * valor.innerHTML).toFixed(2);
+    if (acao == '-' && qtd.innerHTML == 0) {
+        
+    } else {
+        acao == '+' ? qtd.innerHTML++ : qtd.innerHTML--;
+        total.innerHTML = (qtd.innerHTML * valor.innerHTML).toFixed(2);
+        soma();
+    }
+}
+
+function soma() {
+    let soma = 0;
+
+    for(let i = 1; i < 4; i++) {
+        soma += Number(document.getElementById('total' + i).innerHTML);
+    }
+
+    document.getElementById('subTotal').innerHTML = soma.toFixed(2);
 }
